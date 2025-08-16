236c8756f8c99176d7ed1fa01a60a37003ef9b51
ReshmaShaik20 <reshmashaik03881@gmail.com>
2025-08-16 11:53:56 +0530

    Create script.js

---
+++ core/static/js/script.js
@@ -0,0 +1,5 @@
+// MySocialApp/core/static/js/script.js
+console.log("Welcome to My Social App!");
+
+// You can add more interactive JavaScript here later!
+// For now, it's just a friendly message in the browser's console.
